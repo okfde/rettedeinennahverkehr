09f04ea6ac9389f1e7b33a62d9ac4e7c6ee5d689
Maximilian Richt <maxi@richt.name>
2017-02-11 14:09:33 +0100

    add fallback if no data is available

--- js/lobbyingo.js
+++ js/lobbyingo.js
@@ -6,8 +6,13 @@ function renderPDF() {
 	var name= $("#name").val();
 	var address= $("#address").val();
 	var city = $("#city").val();
+  var intro = '';
 
-	var text = (sge + " " + ar + " " + lnn + ",\n\n" +
+  if (sge.length > 0 && ar.length > 0 && lnn.length > 0) {
+    intro = sge + " " + ar + " " + lnn + ",";
+  }
+
+	var text = (intro + "\n\n" +
     "mit einiger Empörung durfte ich der Lügenpresse entnehmen, dass der hiesige Verkehrsverbund seine Fahrplandaten " +
     "zwar mit Google teilt, nicht jedoch mit einheimischen Entwickler*innen. Sicher verstehen Sie, dass es auf diese " +
     "Weise mit Industrie 4.0 in Deutschland und moderner Mobilität nix werden kann.\n\nIch würde mich sehr freuen, " +
@@ -18,13 +23,18 @@ function renderPDF() {
       months = 'Januar,Februar,März,April,Mai,Juni,Juli,August,September,Oktober,November,Dezember'.split(','),
       datum = d.getDate() + ". " + months[d.getMonth()] + " "+ d.getFullYear();
 
+  var sender = "\n\n";
+  if (name.length > 0 && address.length > 0 && city.length > 0) {
+    sender = name + "\n" + address + "\n" + plz + " " + city;
+  }
+
 	var doc = new jsPDF();
 	doc.setFontSize(13);
   doc.text(20,  46, lra)
 	doc.text(20,  56, arn + " " + lvn + " " + lnn)
 	doc.text(20,  64, str);
 	doc.text(20,  74, lraplz + " " + stt);
-	doc.text(150,  30, name + "\n" + address + "\n" + plz + " " + city + "\n\n" + datum)
+	doc.text(150,  30, sender + "\n\n" + datum)
 
 	lines = doc.splitTextToSize(text, 160)
 	doc.text(20, 100, lines)
@@ -32,7 +42,7 @@ function renderPDF() {
 	doc.line(20, 215, 80, 215);
 	doc.text(20, 222, "name");
 
-	doc.output("datauri");
+	doc.output("dataurlnewwindow");
 }
 
 $(document).ready(function() {
@@ -41,6 +51,7 @@ $(document).ready(function() {
   $('#plz').keyup(function() {
     plz = $(this).val();
     if (plz.length <= 4) {
+      $('#actionError').hide();
       $('#actionResult').hide();
       return;
     }
@@ -74,10 +85,15 @@ $(document).ready(function() {
         tel = response.tel;
 
         $('#tel').text(tel);
+        $('#tel-name').text(arn + " " + lvn + " " + lnn);
         $('#actionResult').show();
+      }).fail(function() {
+        $('#actionError').show();
       });
+    }).fail(function() {
+      $('#actionError').show();
     });
   });
 
-  $('#actionMailBtn').click(renderPDF);
+  $('.action-generate-pdf').click(renderPDF);
 });
