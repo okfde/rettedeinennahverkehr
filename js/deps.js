9dbf00e1556303f573c3142bdabc4fdf121ee28f
Maximilian Richt <maxi@richt.name>
2017-02-13 00:58:57 +0100

    cleanup and combine resources

---
+++ js/deps.js
@@ -0,0 +1,11 @@
+---
+layout: null
+---
+
+{% include_relative libs/jquery-1.11.0.js %}
+{% include_relative libs/bootstrap.min.js %}
+{% include_relative libs/jquery.easing.min.js %}
+{% include_relative libs/classie.js %}
+{% include_relative libs/cbpAnimatedHeader.js %}
+{% include_relative libs/jspdf-1.3.2.min.js %}
+{% include_relative libs/tabletop-1.5.1.min.js %}
